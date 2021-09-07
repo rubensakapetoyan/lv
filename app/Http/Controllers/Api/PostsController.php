127342a3440e602a6bba7360ba9ba92a102542fc
leonidkotanidi <leocotanidi@gmail.com>
2021-09-08 03:05:21 +0400

    [ FEATURE * ] - second_changes2 - new changes

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -3,43 +3,64 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\SendSubscribtionEmail;
 use Illuminate\Http\Request;
 use App\Services\PostsServices;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Notice;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
     public $postService;
+    public $post;
+    public $notice;
 
-    public function __construct(PostsServices $postService) {
+    public function __construct(PostsServices $postService, Post $post, Notice $notice) {
         $this->postService = $postService;
+        $this->post = $post;
+        $this->notice = $notice;
     }
 
     public function createPost(Request $request) {
 
-        /*$validated = Validator::make($request->all(), [
+        $validated = Validator::make($request->all(), [
             'title' => 'required|unique:posts|max:255',
             'content' => 'required',
             'website' => 'integer',
             'user' => 'integer'
-        ]);*/
+        ]);
         /*implode(' , ', count($validated->errors()->all())
                     ? $validated->errors()->all() : [''])*/
-        /*if(count($validated->errors())) {
+        if(count($validated->errors())) {
             return response()->json([
                 'status' => 'o',
                 'errors' => $validated->errors()->all()
-
             ])->setStatusCode('503');
-        }*/
+        }
 
-//        $result = @$this->postService->create($request)->id;
+        $result = @$this->postService->create($request);
 
-        return $this->postService->setToJob(12);
+        if($result) {
+            $arr = $this->postService->setToJob();
+            foreach($arr as $s) { 
+                if(!$this->notice->where('noticer', $s->email)->get()) {
+                    $this->notice::create([
+                       'noticer' =>  $s->email
+                    ]);
+                }
+                if(!$this->notice->where('noticer', $s->email)->get()->noticed) {
+                    $this->dispatch(new SendSubscribtionEmail($s->email, $result->title, $result->content));
+                    $this->notice::create([
+                        'noticed' =>  $s->email
+                    ]);
+                }
+            }
+        }
 
         return response()->json([
             'status' => 'ok',
-            'postId' => '12'//$result
+            'postId' => @$result->id
         ])->setStatusCode('200');
 
     }
